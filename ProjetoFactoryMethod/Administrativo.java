007ab4798ad7fd5fe6dfa8fa576d53bfbca21e10
jvitor442 <joaovit.442@gmail.com>
2023-03-09 21:18:21 -0300

    Adicionado alguns ajustes

--- ProjetoFactoryMethod/Administrativo.java
+++ ProjetoFactoryMethod/Administrativo.java
@@ -2,7 +2,7 @@ package TecnicasDeProgramacaoII.ProjetoFactoryMethod;
 
 public class Administrativo extends Pessoa {
 
-    public String relacao(){
+    public String relacao() {
         return " tem relação com a instituição como: Administrativo";
     }
 }
--- ProjetoFactoryMethod/Validando.java
+++ ProjetoFactoryMethod/Validando.java
@@ -1,33 +1,30 @@
 package TecnicasDeProgramacaoII.ProjetoFactoryMethod;
 
 public class Validando {
-    public String validar(String cargo){
-        if(cargo.intern() == "administrativo"){
+    public String validar(String cargo) {
+        if (cargo.intern() == "administrativo") {
             Administrativo adm = new Administrativo();
             return adm.relacao();
-        }
-        else if(cargo.intern() == "aluno"){
+        } else if (cargo.intern() == "aluno") {
             Aluno aluno = new Aluno();
             return aluno.relacao();
-        }
-        else if(cargo.intern() == "coordenador"){
+        } else if (cargo.intern() == "coordenador") {
             Coordenador coord = new Coordenador();
             return coord.relacao();
-        }
-        else if(cargo.intern() == "diretor"){
+        } else if (cargo.intern() == "diretor") {
             Diretor diretor = new Diretor();
             return diretor.relacao();
-        }
-        else if(cargo.intern() == "professor"){
+        } else if (cargo.intern() == "portaria") {
+            Portaria port = new Portaria();
+            return port.relacao();
+        } else if (cargo.intern() == "professor") {
             Professor prof = new Professor();
             return prof.relacao();
-        }
-        else if(cargo.intern() == "vestibulando"){
+        } else if (cargo.intern() == "vestibulando") {
             Vestibulando vest = new Vestibulando();
             return vest.relacao();
-        }
-        else{
+        } else {
             return " não tem nenhuma relação com a instituição, acompanhar até a secretaria";
         }
-    } 
+    }
 }
